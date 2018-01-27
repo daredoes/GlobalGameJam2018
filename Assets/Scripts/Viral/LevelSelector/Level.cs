d8b586b77697795f913912f5a161f50793c20f89
RonNofar <ronnofar2@gmail.com>
2018-01-27 12:54:19 -0800

    Update LevelGraph
    Changing up the way to set up the location graph.

--- Assets/Scripts/Viral/LevelSelector/Level.cs
+++ Assets/Scripts/Viral/LevelSelector/Level.cs
@@ -7,18 +7,39 @@ namespace Viral
     public class Level
     {
 
-        public enum Location
+        public struct Location
         {
-            NULL = -1,
-            BRAIN = 0,
-            SPINE = 1,
-            COLON = 2,
-            BICEPTS = 3,
-            TRICEPTS = 4,
-            PECTORALS = 5,
-            ABDOMEN = 6,
-            QUADRICEPS = 7,
-            CALVES = 8
+            public enum Type
+            {
+                NULL = -1,
+                BRAIN = 0,
+                SPINE = 1,
+                COLON = 2,
+                HEART = 3,
+                BICEPTS = 4,
+                TRICEPTS = 5,
+                PECTORALS = 6,
+                ABDOMEN = 7,
+                QUADRICEPS = 8,
+                CALVES = 9
+            }
+
+            public enum Side
+            {
+                NULL = -1,
+                NONE = 0,
+                LEFT = 1,
+                RIGHT = 2
+            }
+
+            public Type type;
+            public Side side;
+
+            public Location(Type type, Side side)
+            {
+                this.type = type;
+                this.side = side;
+            }
         }
 
         public enum Type
@@ -39,6 +60,7 @@ namespace Viral
 
         public string name;
         public Location location;
+        public Side side;
         public Type type;
         public Priority priority;
         public int daysLeft;
--- Assets/Scripts/Viral/LevelSelector/LevelGraph.cs
+++ Assets/Scripts/Viral/LevelSelector/LevelGraph.cs
@@ -15,6 +15,16 @@ namespace Viral
         {
             public float weight; // 3 for high priority, 2 for medium, 1 for low
             public Level.Location endLocation;
+
+            public Vertex(
+                Level.Location.Type endLocationType, 
+                Level.Location.Side endLocationSide, 
+                float weight)
+            {
+                this.weight = weight;
+                this.endLocation = 
+                    new Level.Location(endLocationType, endLocationSide);
+            }
         }
 
         public Dictionary<Level.Location, Node> graph;
@@ -22,40 +32,108 @@ namespace Viral
         public LevelGraph()
         {
             graph = new Dictionary<Level.Location, Node>();
-            foreach (Level.Location location in System.Enum.GetValues(typeof(Level.Location)))
+            foreach (Level.Location.Type location in System.Enum.GetValues(typeof(Level.Location)))
             {
                 Node node = new Node();
                 List<Vertex> verts = new List<Vertex>();
                 Vertex vert = new Vertex();
                 switch (location) {
-                    case Level.Location.NULL:
+                    case Level.Location.Type.NULL:
                         Debug.Log("ERROR: NULL value.");
                         break;
-                    case Level.Location.BRAIN:
-                        vert.endLocation = Level.Location.SPINE;
-                        vert.weight = 3;
-                        verts.Add(vert);
-                        node.vertices = verts;
-                        graph.Add(location, node);
+                    case Level.Location.Type.BRAIN:
+                        graph.Add(
+                            new Level.Location(location, Level.Location.Side.NONE),
+                            AddVertexArrayToNode(
+                                new Vertex[] {
+                                    new Vertex(
+                                        Level.Location.Type.SPINE, 
+                                        Level.Location.Side.NONE,
+                                        3)
+                                }));
                         break;
-                    case Level.Location.SPINE:
-                        vert.endLocation = Level.Location.BRAIN;
-                        vert.weight = 3;
-                        verts.Add(vert);
-                        vert.endLocation = Level.Location.PECTORALS;
-                        vert.weight = 2;
-                        verts.Add(vert);
-                        vert.endLocation = Level.Location.BICEPTS;
-                        vert.weight = 1;
-                        verts.Add(vert);
+                    case Level.Location.Type.SPINE:
+                        graph.Add(
+                            new Level.Location(location, Level.Location.Side.NONE),
+                            AddVertexArrayToNode(
+                                new Vertex[] {
+                                    new Vertex(
+                                        Level.Location.Type.BRAIN,
+                                        Level.Location.Side.NONE,
+                                        3),
+                                    new Vertex(
+                                        Level.Location.Type.PECTORALS, 
+                                        Level.Location.Side.LEFT,
+                                        2),
+                                    new Vertex(
+                                        Level.Location.Type.PECTORALS,
+                                        Level.Location.Side.RIGHT, 
+                                        2)
+                                }));
                         break;
-                    case Level.Location.COLON:
+                    case Level.Location.Type.COLON:
+                        graph.Add(
+                            new Level.Location(location, Level.Location.Side.NONE),
+                            AddVertexArrayToNode(
+                                new Vertex[] {
+                                    new Vertex(
+                                        Level.Location.Type.HEART,
+                                        Level.Location.Side.NONE,
+                                        3),
+                                    new Vertex(
+                                        Level.Location.Type.ABDOMEN,
+                                        Level.Location.Side.NONE,
+                                        2)
+                                }));
                         break;
-                    case 3:
+                    case Level.Location.Type.HEART:
+                        graph.Add(
+                            new Level.Location(location, Level.Location.Side.NONE),
+                            AddVertexArrayToNode(
+                                new Vertex[] {
+                                    new Vertex(
+                                        Level.Location.Type.COLON,
+                                        Level.Location.Side.NONE,
+                                        2),
+                                    new Vertex(
+                                        Level.Location.Type.PECTORALS,
+                                        Level.Location.Side.LEFT,
+                                        1),
+                                    new Vertex(
+                                        Level.Location.Type.PECTORALS,
+                                        Level.Location.Side.RIGHT,
+                                        1)
+                                }));
                         break;
-                    case 4:
+                    case Level.Location.Type.BICEPTS:
+                        foreach (Level.Location.Side side in System.Enum.GetValues(typeof(Level.Location.Side)))
+                        {
+                            switch (side) {
+                                case Level.Location.Side.NULL:
+                                    break;
+                                case Level.Location.Side.NONE:
+                                    break;
+                                case Level.Location.Side.LEFT:
+
+                                case Level.Location.Side.RIGHT:
+                                    graph.Add(
+                                    new Level.Location(location, side),
+                                    AddVertexArrayToNode(
+                                        new Vertex[] {
+                                            new Vertex(
+                                                Level.Location.Type.PECTORALS,
+                                                side,
+                                                2),
+                                            new Vertex(
+                                                Level.Location.Type.TRICEPTS,
+                                                side,
+                                                1)
+                                        }));
+                                    break;
+                            }
+                        }
                         break;
-                    case 5:
+                    case Level.Location.Type.TRICEPTS:
                         break;
                     case 6:
                         break;
@@ -68,6 +146,20 @@ namespace Viral
         public LevelGraph(Dictionary<Level.Location, Node> dictionary)
         {
             graph = dictionary;
+        }
+
+        private Node AddVertexArrayToNode(Vertex[] vertex)
+        {
+            Node node = new Node();
+            List<Vertex> vertices = new List<Vertex>();
+
+            for (int i = 0; i < vertex.Length; ++i)
+            {
+                vertices.Add(vertex[i]);
+            }
+            node.vertices = vertices;
+
+            return node;
         }
     }
 }
