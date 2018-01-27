dbc8ae792ef9989281c9b1e59bb1617470ff0d46
Prince Christian Basiga <princebasiga@gmail.com>
2018-01-27 07:27:39 -0800

    Updated WalkSuperUpdate

--- Assets/Scripts/Viral/ControlSystem/PlayermMachine.cs
+++ Assets/Scripts/Viral/ControlSystem/PlayermMachine.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/*
+
 namespace Viral.ControlSystem
 {
     public class PlayerMachine : ControllerStateMachine
@@ -19,13 +19,20 @@ namespace Viral.ControlSystem
             Fall
         }
 
+
+
         #region VARS
+
+        int normalizedHorizontalSpeed;
+        float groundDamping = 20f;
+        float inAirDamping = 5f;
+        float gravity = -25f;
+        Vector3 velocity;
+
+
         [SerializeField]
         private InputController _input;
-        private GameObject _pickUpButton;
-        private float timeSinceLastAerialAttack = 0f;
-        private float aerialAttackThreshHold = 0.5f;
-        private bool aerialAttackStarted = false;
+       
         #endregion
 
         #region PROPERTIES
@@ -41,17 +48,7 @@ namespace Viral.ControlSystem
             }
         }
 
-        GameObject pickUpButton
-        {
-            get
-            {
-                if (_pickUpButton == null)
-                {
-                    //_pickUpButton = Mobile.MobileController.controller.buttons.PickUp.gameObject;
-                }
-                return _pickUpButton;
-            }
-        }
+      
 
         private Vector3 LocalMovement
         {
@@ -88,15 +85,7 @@ namespace Viral.ControlSystem
             //Flip the character
             if ((Input.Current.FlipInput) || (Input.Current.MoveInput.x > 0 && !facingRight) || (Input.Current.MoveInput.x < 0 && facingRight)) { Flip(); }
 
-            if (Input.Current.ItemPickUp)
-            {
-                PickUpItem();
-            }
-
-            if (Input.Current.OpenInventory)
-            {
-                OpenInventory();
-            }
+          
         }
 
         protected override void LateGlobalSuperUpdate()
@@ -124,7 +113,6 @@ namespace Viral.ControlSystem
            // Controller.EnableSlopeLimit();
            // Controller.EnableClamping();
             grounded = true;
-            aerialAttackStarted = false;
             //anim.SetBool(GameConstants.ANIM_GROUNDED, grounded);
             EnableShadow(true);
             //EnableDust(grounded);
@@ -181,7 +169,8 @@ namespace Viral.ControlSystem
             }
 
             // Apply friction to slow us to a halt
-            moveDirection = Vector3.MoveTowards(moveDirection, Vector3.zero, walkDecceleration * Controller.deltaTime);
+         //   moveDirection = Vector3.MoveTowards(moveDirection, Vector3.zero, walkDecceleration * Controller.deltaTime);
+
         }
 
         void Walk_EnterState()
@@ -198,6 +187,7 @@ namespace Viral.ControlSystem
 
         void Walk_SuperUpdate()
         {
+           
             if (IsHit)
             {
                 ApplyDamageType();
@@ -213,339 +203,58 @@ namespace Viral.ControlSystem
             if (!MaintainingGround)
             {
                 currentState = PlayerStates.Fall;
-                return;
-            }
-
-            if (Input.Current.BlockInput)
-            {
-                currentState = PlayerStates.Block;
-                return;
-            }
-
-            if (Input.Current.AttackInput)
-            {
-                currentState = PlayerStates.Attack_Melee;
-                return;
-            }
-
-            if (Input.Current.MagicInput)
-            {
-                currentState = PlayerStates.Attack_Magic;
-                return;
-            }
-
-            if (Input.Current.ThrowInput)
-            {
-                currentState = PlayerStates.Throw;
-                return;
-            }
-
-            if (Input.Current.MoveInput != Vector3.zero)
-            {
-                //moveDirection = Vector3.MoveTowards(moveDirection, LocalMovement * speed, walkAcceleration * Controller.deltaTime);
             }
             else
             {
+                velocity.y = 0;
+            }
+        
+            //But input stuff shouldn't be here, so these in separate methods?
+            if (Input.Current.MoveInput == Vector3.right)
+            {
+                normalizedHorizontalSpeed = 1;
+
+                if (transform.localScale.x < 0f)
+                    transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+                //moveDirection = Vector3.MoveTowards(moveDirection, LocalMovement * speed, walkAcceleration * Controller.deltaTime);
+            }
+            else if (Input.Current.MoveInput == -Vector3.right)
+            {
+                normalizedHorizontalSpeed = -1;
+
+                if (transform.localScale.x > 0f)
+                    transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
+            }
+            else if (Input.Current.MoveInput == Vector3.up)
+            {
+                if (currentState.ToString() != PlayerStates.Fall.ToString() && currentState.ToString() != PlayerStates.Jump.ToString())
+                {
+                    velocity.y = Mathf.Sqrt(2f * jumpHeight * -gravity);
+                }
+
+            }
+            else if (Input.Current.MoveInput == Vector3.zero)
+            {
                 currentState = PlayerStates.Idle;
-                return;
+                normalizedHorizontalSpeed = 0;
             }
 
+            // apply horizontal speed smoothing it. dont really do this with Lerp. Use SmoothDamp or something that provides more control
+            var smoothedMovementFactor = (currentState.ToString() == PlayerStates.Jump.ToString()) ? groundDamping : inAirDamping; // how fast do we change direction?
+            float runSpeed; //Get from StatSystem
+            velocity.x = Mathf.Lerp(velocity.x, normalizedHorizontalSpeed * runSpeed, Time.deltaTime * smoothedMovementFactor);
 
-            EnableDust(grounded);
+
+            velocity.y += gravity * Time.deltaTime;
+            transform.Translate(velocity, Space.World);
+            // EnableDust(grounded);
         }
 
         void Walk_ExitState()
         {
             walking = false;
             anim.SetBool(GameConstants.ANIM_WALKING, walking);
-        }
-
-        void Attack_Melee_EnterState()
-        {
-            if (IsHit)
-            {
-                ApplyDamageType();
-                return;
-            }
-            if (!grounded)
-            {
-                moveDirection = Vector3.zero;
-                attacking_melee = true;
-                aerialAttackStarted = true;
-                timeSinceLastAerialAttack = 0;
-                anim.SetBool(GameConstants.ANIM_GROUNDED, false);
-                anim.SetBool(GameConstants.ANIM_AERIAL, true);
-                Attack();
-            }
-            else
-            {
-                moveDirection = Vector3.zero;
-                attacking_melee = true;
-                Attack();
-            }
-        }
-
-        void Attack_Melee_SuperUpdate()
-        {
-            if (grounded)
-            {
-                if (attacking_melee)
-                {
-                    moveDirection = Vector3.zero;
-                }
-                else
-                {
-                    if (IsHit)
-                    {
-                        ApplyDamageType();
-                        return;
-                    }
-                    if (Input.Current.AttackInput)
-                    {
-                        Attack_Melee_EnterState();
-                        return;
-                    }
-                    currentState = PlayerStates.Idle;
-                    return;
-                }
-                if (IsHit)
-                {
-                    ApplyDamageType();
-                    return;
-                }
-            }
-            else
-            {
-                if (attacking_melee)
-                {
-                    moveDirection -= Controller.up * (gravity * .1f) * Controller.deltaTime;
-                }
-                else
-                {
-                    if (Input.Current.AttackInput)
-                    {
-                        if (aerialAttackStarted)
-                        {
-                            if (timeSinceLastAerialAttack < aerialAttackThreshHold)
-                            {
-                                Attack_Melee_EnterState();
-                                return;
-                            }
-                            else
-                            {
-                                Debug.Log("CAN'T DO IT");
-                            }
-                        }
-                        else
-                        {
-                            Attack_Melee_EnterState();
-                            return;
-                        }
-                    }
-                    currentState = PlayerStates.Fall;
-                    return;
-                }
-            }
-        }
-
-        void Attack_Magic_EnterState()
-        {
-            if (!IsActive) { return; }
-            if (statCollection.GetStat<StatRegeneratable>(StatType.Magic).Value < 8)
-            {
-                currentState = PlayerStates.Idle;
-                return;
-            }
-            moveDirection = Vector3.zero;
-            attacking_magic = true;
-            Magic();
-        }
-
-        void Attack_Magic_SuperUpdate()
-        {
-            moveDirection = Vector3.zero;
-            if (!attacking_magic)
-            {
-                if (!MaintainingGround)
-                {
-                    currentState = PlayerStates.Fall;
-                    return;
-                }
-                currentState = PlayerStates.Idle;
-                return;
-            }
-        }
-
-        void Throw_EnterState()
-        {
-            if (IsHit)
-            {
-                ApplyDamageType();
-                return;
-            }
-
-            switch (inventoryManager.throwableInUse)
-            {
-                case InventoryManager.ThrowableInUse.Projectile:
-                    if (inventoryManager.LauncherSlotEquipped && inventoryManager.ProjectileSlotEquipped)
-                    {
-                        if (inventoryManager.Projectile.StackSize > 0)
-                        {
-                            moveDirection = Vector3.zero;
-                            throwing = true;
-                            if (!grounded)
-                            {
-                                anim.SetBool(GameConstants.ANIM_GROUNDED, false);
-                                anim.SetBool(GameConstants.ANIM_AERIAL, true);
-                            }
-                            anim.SetTrigger(GameConstants.ANIM_BOW);
-                            return;
-                        }
-                    }
-                    break;
-                case InventoryManager.ThrowableInUse.Throwable:
-                    if (inventoryManager.ThrowableSlotEquipped)
-                    {
-                        if (inventoryManager.Throwable.StackSize > 0)
-                        {
-                            moveDirection = Vector3.zero;
-                            throwing = true;
-                            if (!grounded)
-                            {
-                                anim.SetBool(GameConstants.ANIM_AERIAL, true);
-                            }
-                            anim.SetTrigger(GameConstants.ANIM_THROW);
-                            return;
-                        }
-                    }
-                    break;
-            }
-        }
-
-        void Throw_SuperUpdate()
-        {
-            if (grounded)
-            {
-                if (throwing)
-                {
-                    moveDirection = Vector3.zero;
-                }
-                else
-                {
-                    if (IsHit)
-                    {
-                        ApplyDamageType();
-                        return;
-                    }
-
-                    currentState = PlayerStates.Idle;
-                    return;
-                }
-            }
-            else
-            {
-                if (throwing)
-                {
-                    // Move down slower
-                    moveDirection -= Controller.up * (gravity * .1f) * Controller.deltaTime;
-                }
-                else
-                {
-                    if (IsHit)
-                    {
-                        ApplyDamageType();
-                        return;
-                    }
-
-                    currentState = PlayerStates.Fall;
-                    return;
-                }
-            }
-            if (IsHit)
-            {
-                ApplyDamageType();
-                return;
-            }
-
-        }
-
-        void Block_EnterState()
-        {
-            blocking = true;
-            anim.SetBool(GameConstants.ANIM_BLOCKING, blocking);
-        }
-
-        void Block_SuperUpdate()
-        {
-            moveDirection = Vector3.zero;
-
-            if (!Input.Current.BlockInput)
-            {
-                currentState = PlayerStates.Idle;
-                return;
-            }
-        }
-
-        void Block_ExitState()
-        {
-            blocking = false;
-            anim.SetBool(GameConstants.ANIM_BLOCKING, blocking);
-        }
-
-        void Juggle_EnterState()
-        {
-            anim.SetBool("KnockBack", true);
-            Controller.DisableClamping();
-            Controller.DisableSlopeLimit();
-            grounded = false;
-            EnableShadow(grounded);
-            EnableDust(grounded);
-            moveDirection += Controller.up * CalculateKnockBackSpeed(2, gravity);
-            anim.SetFloat(GameConstants.ANIM_VERTICAL_SPEEED, moveDirection.y);
-
-            if (IsHit)
-            {
-                ApplyDamageType();
-                return;
-            }
-        }
-
-        void Juggle_SuperUpdate()
-        {
-            Vector3 planarMoveDirection = Math3d.ProjectVectorOnPlane(Controller.up, moveDirection);
-            Vector3 verticalMoveDirection = moveDirection - planarMoveDirection;
-
-            if (Vector3.Angle(verticalMoveDirection, Controller.up) > 90 && AcquiringGround)
-            {
-                moveDirection = planarMoveDirection;
-                EmitLandedParticles();
-                currentState = PlayerStates.Idle;
-                return;
-            }
-
-            if (IsHit)
-            {
-                ApplyDamageType();
-                return;
-            }
-
-            planarMoveDirection = Vector3.MoveTowards(planarMoveDirection, (damageDirection / 3f) * _baseSpeed, jumpAcceleration * Controller.deltaTime);
-            verticalMoveDirection -= Controller.up * gravity * Controller.deltaTime;
-            moveDirection = planarMoveDirection + verticalMoveDirection;
-            anim.SetFloat(GameConstants.ANIM_VERTICAL_SPEEED, moveDirection.y);
-        }
-
-        void Juggle_ExitState()
-        {
-            if (entity.IsDead)
-            {
-                entity.FlashSprites();
-                //leave in knockback state
-            }
-            else
-            {
-                anim.SetBool("KnockBack", false);
-            }
         }
 
         void Jump_EnterState()
@@ -619,23 +328,7 @@ namespace Viral.ControlSystem
                 return;
             }
 
-            if (Input.Current.AttackInput)
-            {
-                currentState = PlayerStates.Attack_Melee;
-                return;
-            }
-
-            if (Input.Current.MagicInput)
-            {
-                currentState = PlayerStates.Attack_Melee;
-                return;
-            }
-
-            if (Input.Current.ThrowInput)
-            {
-                currentState = PlayerStates.Throw;
-                return;
-            }
+            
 
             moveDirection -= Controller.up * gravity * Controller.deltaTime;
             anim.SetFloat(GameConstants.ANIM_VERTICAL_SPEEED, moveDirection.y);
@@ -715,41 +408,7 @@ namespace Viral.ControlSystem
             }
         }
 
-        /// <summary>
-        /// Opens the Inventory UI
-        /// </summary>
-        void OpenInventory()
-        {
-            PlayerPanels.instance.Open(entity);
-        }
-
-        private void ItemEncountered(object sender = null, System.EventArgs e = null)
-        {
-            EnablePickUp();
-        }
-
-        private void ItemLeft(object sender = null, System.EventArgs e = null)
-        {
-            DisablePickUp();
-        }
-
-        void EnablePickUp()
-        {
-            pickUpButton.GetComponentInChildren<UnityEngine.UI.Text>().text = inventoryManager.currentItemObject.GetComponent<ItemCollectable>().MyItem.Name;
-            pickUpButton.SetActive(true);
-        }
-
-        void DisablePickUp()
-        {
-            pickUpButton.SetActive(false);
-        }
-
-        void PickUpItem()
-        {
-            inventoryManager.PickUpItem();
-        }
+       
         #endregion
     }
 }
-
-*/
