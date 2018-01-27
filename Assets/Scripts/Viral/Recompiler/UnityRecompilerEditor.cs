aad9ff733af05554fbcb48f47007e8b222f3e198
RonNofar <ronnofar2@gmail.com>
2018-01-27 02:11:53 -0800

    Update GUIManager
    Added UnityRecompiler script, editor script and prefab. Updated GUIManager. Added StartMenuPanelHandler script.

---
+++ Assets/Scripts/Viral/Recompiler/UnityRecompilerEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomEditor(typeof(UnityRecompiler))]
+public class UnityRecompilerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        UnityRecompiler myScript = (UnityRecompiler)target;
+        if (GUILayout.Button("Refresh"))
+        {
+            myScript.Refresh();
+        }
+        if (GUILayout.Button("Recompile All Assets"))
+        {
+            myScript.RecompileAllAssets();
+        }
+    }
+}
+#endif
---
+++ Assets/Scripts/Viral/UI/Editor/GUIManagerEditor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+namespace Viral
+{
+    [CustomEditor(typeof(GUIManager))]
+    public class GUIManagerEditor : Editor
+    {
+        [Header("Editor")]
+        bool fold = true;
+
+        public int panelIndex = 0;
+
+        private string[] panels;
+
+        private void ReloadList()
+        {
+            GUIManager gui = GUIManager.Instance;
+
+            //Debug.Log(gui);
+            gui.PopulatePanelListComplete();
+            panels = null;
+            panels = gui.panelList.Select(I => I.name).ToArray();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            fold = EditorGUILayout.InspectorTitlebar(fold, GUIManager.Instance);
+
+            if (fold)
+            {
+                EditorGUILayout.Space();
+
+                ReloadList();
+                int oldPanelIndex = panelIndex;
+                panelIndex = EditorGUILayout.Popup("Panel", panelIndex, panels, EditorStyles.popup);
+                if (panelIndex != oldPanelIndex)
+                    GUIManager.Instance.TogglePanel(GUIManager.Instance.panelList[panelIndex]);
+
+                GUIManager script = (GUIManager)target;
+                if (GUILayout.Button("TogglePanel"))
+                {
+                    GUIManager.Instance.TogglePanel(GUIManager.Instance.panelList[panelIndex]);
+                }
+            }
+        }
+    }
+#endif
+}
